0d4bd0c0f532e35dffab9fba8eb270f61db38ead
robertzml <robertzml@126.com>
2016-06-23 22:12:51 +0800

    Add Poseidon.Base project. Add Poseidon.Data project.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.Base/Model/BaseEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poseidon.Base.Model
+{
+    /// <summary>
+    /// 基础对象类
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 标识
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int Status { get; set; }
+    }
+}
