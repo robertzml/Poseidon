42bf625606d6965ccb77f1bfe9ec6aae22f384e2
robertzml <robertzml@126.com>
2016-06-28 20:04:59 +0800

    Add sql db files.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.Data/IBaseDAL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poseidon.Data
+{
+    /// <summary>
+    /// 数据访问层通用接口
+    /// </summary>
+    public interface IBaseDAL
+    {
+    }
+}
---
+++ Poseidon.Data/IBaseDALUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poseidon.Data
+{
+    public interface IBaseDALUtil
+    {
+        string GetConnectionString();
+    }
+}
