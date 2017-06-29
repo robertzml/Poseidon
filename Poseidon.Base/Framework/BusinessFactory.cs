edc598a09caf0847d117fc613af29cb2ff2b0e39
robertzml <robertzml@126.com>
2017-06-30 01:15:42 +0800

    Add BusinessTest.
    Signed-off-by: robertzml <robertzml@126.com>

--- Poseidon.Base/Framework/BusinessFactory.cs
+++ Poseidon.Base/Framework/BusinessFactory.cs
@@ -15,9 +15,14 @@ namespace Poseidon.Base.Framework
     {
         #region Field
         /// <summary>
+        /// 缓存
+        /// </summary>
+        private static Cache objCache = Cache.Instance;
+
+        /// <summary>
         /// 业务类缓存
         /// </summary>
-        private static Hashtable objCache = new Hashtable();
+        //private static Hashtable objCache = new Hashtable();
 
         /// <summary>
         /// 锁变量
@@ -29,11 +34,21 @@ namespace Poseidon.Base.Framework
         /// <summary>
         /// 创建对应业务类的实例，不缓存
         /// </summary>
+        /// <returns></returns>
+        public static T GetInstance()
+        {
+            T bll = Reflect<T>.Create(typeof(T).FullName, typeof(T).Assembly.GetName().Name, false);
+            return bll;
+        }
+
+        /// <summary>
+        /// 创建对应业务类的实例，不缓存
+        /// </summary>
         /// <param name="args">构造函数参数</param>
         /// <returns></returns>
         public static T GetInstance(object[] args)
         {
-            T bll = Reflect<T>.Create(typeof(T).FullName, typeof(T).Assembly.GetName().Name, args, false); 
+            T bll = Reflect<T>.Create(typeof(T).FullName, typeof(T).Assembly.GetName().Name, args, false);
             return bll;
         }
         #endregion //Method
@@ -47,24 +62,21 @@ namespace Poseidon.Base.Framework
             get
             {
                 string cacheKey = typeof(T).FullName;
-                //从缓存读取
-                if (objCache.ContainsKey(cacheKey))
+                if (objCache.ContainKey(cacheKey))
                 {
-                    T bll = (T)objCache[cacheKey];
-                    return bll;
+                    return (T)objCache[cacheKey];
                 }
                 else
                 {
                     lock (syncRoot)
                     {
-                        if (objCache.ContainsKey(cacheKey))
+                        if (objCache.ContainKey(cacheKey))
                         {
                             return (T)objCache[cacheKey];
                         }
                         else
                         {
-                            //反射创建，并缓存
-                            T bll = Reflect<T>.Create(typeof(T).FullName, typeof(T).Assembly.GetName().Name);
+                            T bll = Reflect<T>.Create(typeof(T).FullName, typeof(T).Assembly.GetName().Name, false);
                             objCache.Add(cacheKey, bll);
 
                             return bll;
---
+++ Poseidon.Test/Base/BusinessTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Poseidon.Test.Base
+{
+    using Poseidon.Base.Framework;
+    using Poseidon.Common;
+    using Poseidon.Core.BL;
+  
+    /// <summary>
+    /// 业务类测试
+    /// </summary>
+    [TestClass]
+    public class BusinessTest
+    {
+        #region Constructor
+        public BusinessTest()
+        {
+            Cache.Instance.Add("DALPrefix", "Mongo");
+            Cache.Instance.Add("ConnectionString", "mongodb://localhost:27017");
+        }
+        #endregion //Constructor
+
+        #region Test
+        [TestMethod]
+        public void BLTest1()
+        {
+            var bll1 = BusinessFactory<GroupBusiness>.Instance;
+            var bll2 = BusinessFactory<GroupBusiness>.Instance;
+
+            Assert.IsTrue(bll1.Equals(bll2));
+
+            var bll3 = BusinessFactory<GroupBusiness>.GetInstance();
+            Assert.IsFalse(bll1.Equals(bll3));
+        }
+
+        [TestMethod]
+        public void BLTest2()
+        {
+            List<Task<GroupBusiness>> tasks = new List<Task<GroupBusiness>>();
+            int count = 50;
+            for (int i = 0; i < count; i++)
+            {
+                var task = Task.Run(() =>
+                {
+                    var obj = BusinessFactory<GroupBusiness>.Instance;
+                    return obj;
+                });
+
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            var foo0 = tasks[0].Result;
+            var foo1 = tasks[1].Result;
+           
+            Assert.IsTrue(foo0.Equals(foo1));
+
+            for (int i = 1; i < count; i++)
+            {
+                Assert.IsTrue(foo0.Equals(tasks[i].Result));
+            }
+
+
+            foreach (var item in Cache.Instance.GetKeys())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Test finished");
+        }
+
+        [TestMethod]
+        public void BLTest3()
+        {
+            int count = 50;
+            List<Task<GroupBusiness>> tasks = new List<Task<GroupBusiness>>();
+            for (int i = 0; i < count; i++)
+            {
+                var task = Task.Run(() =>
+                {
+                    var obj = new GroupBusiness();
+                    return obj;
+                });
+
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            var foo0 = tasks[0].Result;
+            var foo1 = tasks[1].Result;
+
+            Assert.IsFalse(foo0.Equals(foo1));
+            Console.WriteLine("Test finished");
+        }
+        #endregion //Test
+    }
+}
