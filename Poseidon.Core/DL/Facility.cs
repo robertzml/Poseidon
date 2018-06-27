1d2904afa01898c9c177127b55febd9c96cc60e0
robertzml <robertzml@126.com>
2018-06-27 15:53:35 +0800

    add facility model.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.Core/DL/Facility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poseidon.Core.DL
+{
+    using Poseidon.Base.Framework;
+
+    /// <summary>
+    /// 设施类
+    /// </summary>
+    public class Facility : ObjectEntity
+    {
+        #region Property
+        /// <summary>
+        /// 模型类型
+        /// </summary>
+        [Display(Name = "模型类型")]
+        public string ModelType { get; set; }
+
+        /// <summary>
+        /// 数据集代码
+        /// </summary>
+        [Display(Name = "数据集代码")]
+        public string DatasetCode { get; set; }
+        #endregion //Property
+    }
+}
